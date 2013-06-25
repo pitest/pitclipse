33446f0590359ed742673df6a64e994fa1588b2d
Phil Glover <phil@goldshank.co.uk>
2013-06-25 08:14:22 +0100

    Fixes #5 - Test files have to end *Test for the tycho surefire plugin to find them :(

--- pitclipse-plugin/pitclipse-ui.tests/src/org/pitest/pitclipse/ui/PitclipseStoriesTest.java
+++ pitclipse-plugin/pitclipse-ui.tests/src/org/pitest/pitclipse/ui/PitclipseStoriesTest.java
@@ -2,6 +2,8 @@ package org.pitest.pitclipse.ui;
 
 import static org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences.PLAYBACK_DELAY;
 import static org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences.TIMEOUT;
+import static org.jbehave.core.reporters.Format.CONSOLE;
+import static org.jbehave.core.reporters.Format.HTML;
 import static org.pitest.pitclipse.ui.PitclipseTestActivator.getDefault;
 
 import java.util.List;
@@ -13,7 +15,6 @@ import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.embedder.Embedder;
 import org.jbehave.core.io.LoadFromURL;
 import org.jbehave.core.junit.JUnitStories;
-import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
@@ -26,7 +27,7 @@ import org.pitest.pitclipse.ui.behaviours.steps.ProjectSteps;
 import org.pitest.pitclipse.ui.behaviours.steps.SetupSteps;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
-public class PitclipseStories extends JUnitStories {
+public class PitclipseStoriesTest extends JUnitStories {
 
 	private static final long STORY_TIMEOUT = 10l * 60 * 1000;
 
@@ -37,7 +38,7 @@ public class PitclipseStories extends JUnitStories {
 		return new MostUsefulConfiguration().useStoryLoader(new LoadFromURL())
 				.useStoryReporterBuilder(
 						new StoryReporterBuilder().withDefaultFormats()
-								.withFormats(Format.HTML, Format.CONSOLE)
+								.withFormats(HTML, CONSOLE)
 								.withRelativeDirectory("jbehave-report"));
 	}
 
