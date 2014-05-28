974e0ed678aa88d7a8ec5abe419ac942dcdba08b
Phil Glover <phil@goldshank.co.uk>
2014-05-28 22:57:06 +0100

    Minor tidy up

--- pitclipse-plugin/org.pitest.pitclipse.ui/src/org/pitest/pitclipse/ui/view/mutations/OpenMutationDoubleClick.java
+++ pitclipse-plugin/org.pitest.pitclipse.ui/src/org/pitest/pitclipse/ui/view/mutations/OpenMutationDoubleClick.java
@@ -20,6 +20,7 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.progress.UIJob;
@@ -83,58 +84,7 @@ public enum OpenMutationDoubleClick implements IDoubleClickListener {
 			final String className = mutation.getClassMutations().getClassName();
 			final int lineNumber = mutation.getLineNumber() - 1;
 
-			UIJob job = new UIJob("Opening mutated source") {
-				@Override
-				public IStatus runInUIThread(IProgressMonitor arg0) {
-					return findClass(projectName, className).transform(new Function<IFile, IStatus>() {
-						@Override
-						public IStatus apply(final IFile file) {
-							Optional<IWorkbenchWindow> workbench = Optional.fromNullable(PlatformUI.getWorkbench()
-									.getActiveWorkbenchWindow());
-							if (workbench.isPresent()) {
-								try {
-									IEditorPart editorPart = IDE.openEditor(workbench.get().getActivePage(), file);
-									if (editorPart instanceof ITextEditor && lineNumber >= 0) {
-										ITextEditor textEditor = (ITextEditor) editorPart;
-										IEditorInput editorInput = textEditor.getEditorInput();
-										IDocumentProvider provider = textEditor.getDocumentProvider();
-										provider.connect(editorInput);
-										try {
-											IDocument document = provider.getDocument(editorInput);
-											IRegion line = document.getLineInformation(lineNumber);
-											textEditor.selectAndReveal(line.getOffset(), line.getLength());
-										} catch (BadLocationException e) {
-										} finally {
-											provider.disconnect(editorInput);
-										}
-									}
-								} catch (CoreException e) {
-									return org.eclipse.core.runtime.Status.CANCEL_STATUS;
-								}
-							}
-							return org.eclipse.core.runtime.Status.OK_STATUS;
-						}
-					}).or(org.eclipse.core.runtime.Status.OK_STATUS);
-				}
-
-				private Optional<IFile> findClass(final String projectName, final String className) {
-					IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-					for (IProject project : root.getProjects()) {
-						if (project.getName().equals(projectName) && project.isOpen()) {
-							Optional<IJavaProject> javaProject = Optional.fromNullable(JavaCore.create(project));
-							if (javaProject.isPresent()) {
-								try {
-									IType type = javaProject.get().findType(className);
-									return Optional.fromNullable(root.getFile(type.getPath()));
-								} catch (JavaModelException e) {
-								}
-							}
-						}
-					}
-					return Optional.absent();
-				}
-			};
-			job.schedule();
+			new MutationSelectingJob(projectName, lineNumber, className).schedule();
 			return null;
 		}
 
@@ -143,6 +93,87 @@ public enum OpenMutationDoubleClick implements IDoubleClickListener {
 			return null;
 		}
 
+		private static final class MutationSelectingJob extends UIJob {
+			private final String projectName;
+			private final int lineNumber;
+			private final String className;
+
+			private MutationSelectingJob(String projectName, int lineNumber, String className) {
+				super("Opening mutated source");
+				this.projectName = projectName;
+				this.lineNumber = lineNumber;
+				this.className = className;
+			}
+
+			@Override
+			public IStatus runInUIThread(IProgressMonitor arg0) {
+				return findClass(projectName, className).transform(new OpenFileInEditorAtLine(lineNumber)).or(
+						org.eclipse.core.runtime.Status.OK_STATUS);
+			}
+
+			private Optional<IFile> findClass(final String projectName, final String className) {
+				IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+				for (IProject project : root.getProjects()) {
+					if (project.getName().equals(projectName) && project.isOpen()) {
+						Optional<IJavaProject> javaProject = Optional.fromNullable(JavaCore.create(project));
+						if (javaProject.isPresent()) {
+							try {
+								IType type = javaProject.get().findType(className);
+								return Optional.fromNullable(root.getFile(type.getPath()));
+							} catch (JavaModelException e) {
+							}
+						}
+					}
+				}
+				return Optional.absent();
+			}
+
+			private static final class OpenFileInEditorAtLine implements Function<IFile, IStatus> {
+				private final int lineNumber;
+
+				public OpenFileInEditorAtLine(int lineNumber) {
+					this.lineNumber = lineNumber;
+				}
+
+				@Override
+				public IStatus apply(final IFile file) {
+					Optional<IWorkbenchWindow> workbench = Optional.fromNullable(PlatformUI.getWorkbench()
+							.getActiveWorkbenchWindow());
+					if (workbench.isPresent()) {
+						IWorkbenchWindow wb = workbench.get();
+						try {
+							tryToOpen(wb, file);
+						} catch (CoreException e) {
+							return org.eclipse.core.runtime.Status.CANCEL_STATUS;
+						}
+					}
+					return org.eclipse.core.runtime.Status.OK_STATUS;
+				}
+
+				private void tryToOpen(IWorkbenchWindow workbench, final IFile file) throws PartInitException,
+						CoreException {
+					IEditorPart editorPart = IDE.openEditor(workbench.getActivePage(), file);
+					if (editorPart instanceof ITextEditor && lineNumber >= 0) {
+						ITextEditor textEditor = (ITextEditor) editorPart;
+						IEditorInput editorInput = textEditor.getEditorInput();
+						openEditorAtLine(textEditor, editorInput);
+					}
+				}
+
+				private void openEditorAtLine(ITextEditor textEditor, IEditorInput editorInput) throws CoreException {
+					IDocumentProvider provider = textEditor.getDocumentProvider();
+					provider.connect(editorInput);
+					try {
+						IDocument document = provider.getDocument(editorInput);
+						IRegion line = document.getLineInformation(lineNumber);
+						textEditor.selectAndReveal(line.getOffset(), line.getLength());
+					} catch (BadLocationException e) {
+					} finally {
+						provider.disconnect(editorInput);
+					}
+				}
+			}
+		}
 	}
 
 }
