e7ffd2479083bca254a49ea42d30fe9d60743d1d
Phil Glover <phil@goldshank.co.uk>
2016-07-05 08:11:35 +0100

    Script refactoring

--- prepareRelease.sh
+++ prepareRelease.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-VERSION=$1
+SCRIPT_NAME=$0
+
+function usage {
+  echo "usage: $SCRIPT_NAME version"
+  echo "  version - semantic version number e.g. 1.1.6"
+  exit 1
+}
+
+if [ $# -ne 1 ]; then
+  usage
+fi
+
+USER_VERSION=$1
+TIMESTAMP=`date --utc +'%Y%m%d%H%M'`
+VERSION=$USER_VERSION.$TIMESTAMP
+echo "Preparing release " $VERSION
 cd pitrunner
 mvn versions:set -DnewVersion=$VERSION
 mvn clean install
