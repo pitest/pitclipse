c45e780c75199ce82d67bf0962c3b7be3dd67f74
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-02-06 09:05:54 +0100

    don't use ${SONAR_SCANNER_HOME} for detecting forks
    The variable is set also when building a fork. Use
    ${TRAVIS_SECURE_ENV_VARS} instead

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,12 +4,13 @@
 # Indeed, that means that the build is performed on a branch of the
 # pitest/pitclipse repository and that Travis CI allows us to use
 # encrypted variables (e.g. Sonar's token)
-if [ "${SONAR_SCANNER_HOME}" != "" ]; then
+if [ "$TRAVIS_SECURE_ENV_VARS" = true ]; then
+    echo "Building main repository";
     COMMAND="mvn verify -P jacoco sonar:sonar";
-
 # ELSE we cannot run Sonar analysis because the branch is coming
 # from a fork and Travis CI does not allow use to use encrypted variables.
 else
+    echo "Building a forked repository";
     COMMAND="mvn verify -P jacoco";
 fi
 
