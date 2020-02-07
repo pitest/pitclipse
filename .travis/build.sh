97ec93500c280a2cda4ffb94b047c7fdd49759c0
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-02-07 11:35:04 +0100

    Use TRAVIS_REPO_SLUG for detecting forked repo

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,7 @@
 # Indeed, that means that the build is performed on a branch of the
 # pitest/pitclipse repository and that Travis CI allows us to use
 # encrypted variables (e.g. Sonar's token)
-if [ "$TRAVIS_SECURE_ENV_VARS" = true ]; then
+if [ "${TRAVIS_REPO_SLUG}" = "pitest/pitclipse" ]; then
     echo "Building main repository";
     COMMAND="mvn verify -P jacoco sonar:sonar";
 # ELSE we cannot run Sonar analysis because the branch is coming
