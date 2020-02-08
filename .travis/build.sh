f9717109d8b31211ae0224b93e25f3eea3f4f46d
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-02-08 07:50:15 +0100

    use SONAR_SCANNER_HOME and TRAVIS_REPO_SLUG
    To detect whether Sonar can be run: main repo or internal PR

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# IF the variable is set, we can run Sonar analysis.
+# IF the variables are set, we can run Sonar analysis.
 # Indeed, that means that the build is performed on a branch of the
-# pitest/pitclipse repository and that Travis CI allows us to use
+# pitest/pitclipse repository (or an internal PR) and that Travis CI allows us to use
 # encrypted variables (e.g. Sonar's token)
-if [ "${TRAVIS_REPO_SLUG}" = "pitest/pitclipse" ]; then
-    echo "Building main repository";
+if [ "${SONAR_SCANNER_HOME}" != "" && "${TRAVIS_REPO_SLUG}" = "pitest/pitclipse" ]; then
     COMMAND="mvn verify -P jacoco sonar:sonar";
 # ELSE we cannot run Sonar analysis because the branch is coming
 # from a fork and Travis CI does not allow use to use encrypted variables.
 else
-    echo "Building a forked repository";
     COMMAND="mvn verify -P jacoco";
 fi
 
