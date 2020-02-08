62922700cc21e3adfb92587f013f15444f67030a
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-02-08 07:47:44 +0100

    Revert "always use mvn verify -P jacoco"
    This reverts commit f220e7936ca93f1ecb08b21a825319dad239fa79.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-COMMAND="mvn verify -P jacoco";
+# IF the variable is set, we can run Sonar analysis.
+# Indeed, that means that the build is performed on a branch of the
+# pitest/pitclipse repository and that Travis CI allows us to use
+# encrypted variables (e.g. Sonar's token)
+if [ "${TRAVIS_REPO_SLUG}" = "pitest/pitclipse" ]; then
+    echo "Building main repository";
+    COMMAND="mvn verify -P jacoco sonar:sonar";
+# ELSE we cannot run Sonar analysis because the branch is coming
+# from a fork and Travis CI does not allow use to use encrypted variables.
+else
+    echo "Building a forked repository";
+    COMMAND="mvn verify -P jacoco";
+fi
 
 echo ${COMMAND}
 ${COMMAND}
