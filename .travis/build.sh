fcc8afbc1527ae09febb06942d5921fb406bdf05
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-02-08 07:59:03 +0100

    && condition in bash script

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,8 @@
 # Indeed, that means that the build is performed on a branch of the
 # pitest/pitclipse repository (or an internal PR) and that Travis CI allows us to use
 # encrypted variables (e.g. Sonar's token)
-if [ "${SONAR_SCANNER_HOME}" != "" && "${TRAVIS_REPO_SLUG}" = "pitest/pitclipse" ]; then
+if [ "${SONAR_SCANNER_HOME}" != "" ] && [ "${TRAVIS_REPO_SLUG}" = "pitest/pitclipse" ]
+then
     COMMAND="mvn verify -P jacoco sonar:sonar";
 # ELSE we cannot run Sonar analysis because the branch is coming
 # from a fork and Travis CI does not allow use to use encrypted variables.
