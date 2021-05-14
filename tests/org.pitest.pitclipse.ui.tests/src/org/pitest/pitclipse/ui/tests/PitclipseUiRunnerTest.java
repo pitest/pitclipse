1b59ea227ab8667d1c2e5e5b1113852d47fc31e8
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-05-14 08:42:36 +0200

    85: no need of @AfterClass
    Task-Url: http://github.com/pitest/pitclipse/issues/85

--- tests/org.pitest.pitclipse.ui.tests/src/org/pitest/pitclipse/ui/tests/PitclipseUiRunnerTest.java
+++ tests/org.pitest.pitclipse.ui.tests/src/org/pitest/pitclipse/ui/tests/PitclipseUiRunnerTest.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,11 +32,6 @@ public class PitclipseUiRunnerTest extends AbstractPitclipseSWTBotTest {
         assertNoErrorsInWorkspace();
         openEditor(FOO_CLASS, FOO_BAR_PACKAGE, TEST_PROJECT);
         openEditor(FOO_TEST_CLASS, FOO_BAR_PACKAGE, TEST_PROJECT);
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        deleteAllProjects();
     }
 
     @Test
