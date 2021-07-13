61d57e82d5df1fb7dbac7a72deb974783abe7991
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-07-13 22:02:36 +0200

    136: remove Guava from WorkspaceLevelProjectFinder
    Task-Url: http://github.com/pitest/pitclipse/issues/136

--- bundles/org.pitest.pitclipse.launch/src/org/pitest/pitclipse/launch/config/WorkspaceLevelProjectFinder.java
+++ bundles/org.pitest.pitclipse.launch/src/org/pitest/pitclipse/launch/config/WorkspaceLevelProjectFinder.java
@@ -16,22 +16,20 @@
 
 package org.pitest.pitclipse.launch.config;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableList.Builder;
-
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jdt.core.IJavaProject;
-
-import java.util.List;
-
 import static org.pitest.pitclipse.launch.config.ProjectUtils.getOpenJavaProjects;
 import static org.pitest.pitclipse.launch.config.ProjectUtils.onClassPathOf;
 import static org.pitest.pitclipse.launch.config.ProjectUtils.sameProject;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IJavaProject;
+
 public class WorkspaceLevelProjectFinder implements ProjectFinder {
     @Override
     public List<String> getProjects(LaunchConfigurationWrapper configurationWrapper) throws CoreException {
-        Builder<String> results = ImmutableList.builder();
+        List<String> results = new ArrayList<>();
         IJavaProject testProject = configurationWrapper.getProject();
         List<IJavaProject> projects = getOpenJavaProjects();
         for (IJavaProject project : projects) {
@@ -39,6 +37,6 @@ public class WorkspaceLevelProjectFinder implements ProjectFinder {
                 results.add(project.getProject().getName());
             }
         }
-        return results.build();
+        return results;
     }
 }
