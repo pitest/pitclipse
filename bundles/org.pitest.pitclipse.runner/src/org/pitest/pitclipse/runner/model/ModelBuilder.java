9138f769b594aee8f00daa92ed4167d94c977bc4
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-08-03 16:25:50 +0200

    136: starting removing Guava from ModelBuilder
    Task-Url: http://github.com/pitest/pitclipse/issues/136

--- bundles/org.pitest.pitclipse.runner/src/org/pitest/pitclipse/runner/model/ModelBuilder.java
+++ bundles/org.pitest.pitclipse.runner/src/org/pitest/pitclipse/runner/model/ModelBuilder.java
@@ -16,27 +16,25 @@
 
 package org.pitest.pitclipse.runner.model;
 
-import static com.google.common.collect.Collections2.filter;
-import static com.google.common.collect.ImmutableList.copyOf;
-import static com.google.common.collect.Multimaps.filterKeys;
-import static com.google.common.collect.Multimaps.transformValues;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import org.pitest.pitclipse.runner.PitResults;
 import org.pitest.pitclipse.runner.results.DetectionStatus;
 import org.pitest.pitclipse.runner.results.Mutations;
 
-import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Multimaps;
-import com.google.common.collect.Ordering;
 
 public class ModelBuilder {
 
-    private static final Ordering<Mutation> MUTATION_ORDERING = Ordering.from(MutationSorter.INSTANCE);
     private final ProjectStructureService eclipseStructureService;
 
     public ModelBuilder(ProjectStructureService jdtHelper) {
@@ -73,15 +71,9 @@ public class ModelBuilder {
 
     private List<org.pitest.pitclipse.runner.results.Mutations.Mutation> selectMutationsByStatus(
             Mutations mutations, final DetectionStatus status) {
-        List<org.pitest.pitclipse.runner.results.Mutations.Mutation> allMutations = copyOf(mutations.getMutation());
-        Collection<org.pitest.pitclipse.runner.results.Mutations.Mutation> filtered = filter(allMutations,
-                new Predicate<org.pitest.pitclipse.runner.results.Mutations.Mutation>() {
-                    @Override
-                    public boolean apply(org.pitest.pitclipse.runner.results.Mutations.Mutation mutation) {
-                        return status == mutation.getStatus();
-                    }
-                });
-        return ImmutableList.copyOf(filtered);
+        return mutations.getMutation().stream()
+                .filter(mutation -> status == mutation.getStatus())
+                .collect(Collectors.toList());
     }
 
     private ProjectMutations buildProjectMutation(String project,
@@ -110,31 +102,32 @@ public class ModelBuilder {
 
     private List<ClassMutations> buildClassMutationsFor(final String project,
             List<org.pitest.pitclipse.runner.results.Mutations.Mutation> mutations) {
-        Multimap<String, org.pitest.pitclipse.runner.results.Mutations.Mutation> mutationsByClass = Multimaps.index(
-                mutations, Mutations.Mutation::getMutatedClass);
-        Multimap<String, org.pitest.pitclipse.runner.results.Mutations.Mutation> mutationsForProject = filterKeys(
-                mutationsByClass, new Predicate<String>() {
-                    @Override
-                    public boolean apply(String mutatedClass) {
-                        return eclipseStructureService.isClassInProject(mutatedClass, project);
-                    }
-                });
-        Multimap<String, Mutation> transformedMutations = transformValues(mutationsForProject,
-                dtoMutation -> Mutation.builder().withKillingTest(dtoMutation.getKillingTest())
-                        .withLineNumber(dtoMutation.getLineNumber().intValue())
-                        .withMutatedMethod(dtoMutation.getMutatedMethod())
-                        .withMutator(dtoMutation.getMutator()).withStatus(dtoMutation.getStatus())
-                        .withDescription(dtoMutation.getDescription()).build());
+        Map<String, List<Mutation>> transformedMutations = mutations.stream()
+            .filter(mutatedClass ->
+                eclipseStructureService.isClassInProject(mutatedClass.getMutatedClass(), project))
+            .collect(groupingBy(
+                    Mutations.Mutation::getMutatedClass,
+                    mapping(dtoMutation ->
+                        Mutation.builder().withKillingTest(dtoMutation.getKillingTest())
+                            .withLineNumber(dtoMutation.getLineNumber().intValue())
+                            .withMutatedMethod(dtoMutation.getMutatedMethod())
+                            .withMutator(dtoMutation.getMutator()).withStatus(dtoMutation.getStatus())
+                            .withDescription(dtoMutation.getDescription()).build(),
+                        Collectors.toList())));
         return classMutationsFrom(transformedMutations);
     }
 
-    private List<ClassMutations> classMutationsFrom(Multimap<String, Mutation> mutationsByClass) {
-        ImmutableList.Builder<ClassMutations> builder = ImmutableList.builder();
-        for (String className : mutationsByClass.keySet()) {
-            ImmutableList<Mutation> unsortedMutations = copyOf(mutationsByClass.get(className));
-            ImmutableList<Mutation> sortedMutations = MUTATION_ORDERING.immutableSortedCopy(unsortedMutations);
-            builder.add(ClassMutations.builder().withClassName(className).withMutations(sortedMutations).build());
+    private List<ClassMutations> classMutationsFrom(Map<String, List<Mutation>> mutationsByClass) {
+        List<ClassMutations> classMutations = new ArrayList<>();
+        for (Entry<String, List<Mutation>> entry : mutationsByClass.entrySet()) {
+            List<Mutation> sortedMutations = entry.getValue().stream()
+                    .sorted(MutationSorter.INSTANCE)
+                    .collect(Collectors.toList());
+            classMutations.add(
+                ClassMutations.builder()
+                    .withClassName(entry.getKey())
+                    .withMutations(sortedMutations).build());
         }
-        return builder.build();
+        return classMutations;
     }
 }
