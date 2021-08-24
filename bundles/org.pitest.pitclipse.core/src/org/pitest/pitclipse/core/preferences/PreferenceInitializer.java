f3016b1a18c6cc2c51a098598eb83fed52b6babc
Jonas <50097340+JKutscha@users.noreply.github.com>
2021-08-24 17:02:57 +0200

    Correcting the default init from preferences

--- bundles/org.pitest.pitclipse.core/src/org/pitest/pitclipse/core/preferences/PreferenceInitializer.java
+++ bundles/org.pitest.pitclipse.core/src/org/pitest/pitclipse/core/preferences/PreferenceInitializer.java
@@ -16,15 +16,11 @@
 
 package org.pitest.pitclipse.core.preferences;
 
-import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-import org.eclipse.jface.preference.IPreferenceStore;
-import org.pitest.pitclipse.core.PitCoreActivator;
-import org.pitest.pitclipse.runner.config.PitConfiguration;
-
 import static org.pitest.pitclipse.core.preferences.PitPreferences.AVOID_CALLS_TO;
 import static org.pitest.pitclipse.core.preferences.PitPreferences.EXCLUDED_CLASSES;
-import static org.pitest.pitclipse.core.preferences.PitPreferences.INCREMENTAL_ANALYSIS;
 import static org.pitest.pitclipse.core.preferences.PitPreferences.EXECUTION_MODE;
+import static org.pitest.pitclipse.core.preferences.PitPreferences.INCREMENTAL_ANALYSIS;
+import static org.pitest.pitclipse.core.preferences.PitPreferences.MUTATOR_GROUP;
 import static org.pitest.pitclipse.core.preferences.PitPreferences.RUN_IN_PARALLEL;
 import static org.pitest.pitclipse.core.preferences.PitPreferences.TIMEOUT;
 import static org.pitest.pitclipse.core.preferences.PitPreferences.TIMEOUT_FACTOR;
@@ -32,6 +28,11 @@ import static org.pitest.pitclipse.runner.config.PitConfiguration.DEFAULT_AVOID_
 import static org.pitest.pitclipse.runner.config.PitConfiguration.DEFAULT_MUTATORS;
 import static org.pitest.pitclipse.runner.config.PitConfiguration.DEFAULT_TIMEOUT;
 import static org.pitest.pitclipse.runner.config.PitConfiguration.DEFAULT_TIMEOUT_FACTOR;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.pitest.pitclipse.core.PitCoreActivator;
+import org.pitest.pitclipse.runner.config.PitConfiguration;
 
 /**
  * Class used to initialize default preference values.
@@ -51,7 +52,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
         store.setDefault(RUN_IN_PARALLEL, true);
         store.setDefault(INCREMENTAL_ANALYSIS, false);
         store.setDefault(AVOID_CALLS_TO, DEFAULT_AVOID_CALLS_TO_LIST);
-        store.setDefault(DEFAULT_MUTATORS, "defaultMutators");
+        store.setDefault(MUTATOR_GROUP, DEFAULT_MUTATORS);
         store.setDefault(TIMEOUT, DEFAULT_TIMEOUT);
         store.setDefault(TIMEOUT_FACTOR, DEFAULT_TIMEOUT_FACTOR.toString());
         store.setDefault(EXCLUDED_CLASSES, PitConfiguration.DEFAULT_EXCLUDED_CLASSES);
