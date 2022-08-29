34b7418767e652d02ed306f0b2f32c5c5658caaf
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2022-08-29 14:07:39 +0200

    205: test project with default package
    Task-Url: http://github.com/pitest/pitclipse/issues/205

---
+++ tests/testprojects/org.pitest.pitclipse.testprojects.threeclasses/src/DefaultPackageFoo.java
@@ -0,0 +1,13 @@
+
+
+public class DefaultPackageFoo {
+
+    public int f(int i) {
+        java.util.ArrayList<Object> pointless = new java.util.ArrayList<>();
+        if (pointless.size() == 1)
+            return i + 1;
+        else
+            return 0;
+    }
+
+}
---
+++ tests/testprojects/org.pitest.pitclipse.testprojects.threeclasses/src/foobar/FooTest.java
@@ -0,0 +1,10 @@
+package foobar;
+
+public class FooTest {
+
+    @org.junit.Test public void badTest() {
+        Foo1 x = new Foo1();
+        x.f(1);
+    }
+
+}
